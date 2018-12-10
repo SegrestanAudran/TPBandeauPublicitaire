f426eb3df8a89cfc806a48ef8bcbd4656620448d
asegrest <asegrest@cst-tp-b10303>
2018-12-10 17:27:50 +0100

    commit

---
+++ src/main/java/bandeau/EnNoirEtBlanc.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bandeau;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author asegrest
+ */
+public class EnNoirEtBlanc extends EFFET{
+    private String message;
+    private Bandeau Bandeau;
+    public EnNoirEtBlanc(String message){
+        this.message =message;
+    }
+    
+    public void changementCouleur(){
+        Bandeau.setBackground(Color.black);
+        Bandeau.setForeground(Color.white);
+        Bandeau.setMessage(message);
+    }
+    
+    public void affichesur(){
+        
+    }
+}
