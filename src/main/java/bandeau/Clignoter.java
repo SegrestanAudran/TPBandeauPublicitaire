4649e324ed9a91f813f13d8416a34f873379ff79
Yasmina <Yasmina@LAPTOP-K726GKET.univ-jfc.fr>
2018-12-11 16:59:33 +0100

    commit clignoter

---
+++ src/main/java/bandeau/Clignoter.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bandeau;
+//Le noir et blanc
+import java.awt.Color;
+
+/**
+ *
+ * @author asegrest
+ */
+public class Clignoter extends EFFET{
+    private String message;
+    // private Bandeau Bandeau;
+    private int delaie;
+    
+    public Clignoter(String message,int delaie){
+        this.message =message;
+        this.delaie=delaie;
+    }
+
+    
+    
+    public void afficheSur(Bandeau B1){
+        
+        B1.setBackground(Color.black);
+        B1.setForeground(Color.white);
+        B1.setMessage(message);
+        B1.setForeground(Color.black);
+        B1.sleep(delaie);
+        B1.setForeground(Color.white);
+        B1.sleep(delaie);
+        
+       // B1.setMessage(message);
+    }
+}
+
